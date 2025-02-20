f13672750bce4848608d9bd52ab1c279d69e93fd
thanhdnh <myhoangthanh@yahoo.com>
2025-02-20 14:39:50 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -59,21 +59,61 @@ public class LinkedList
             current = current.link;
         }
     }
+    public int FindMax(){
+        Node current = header.link;
+        int max = int.Parse(current.element.ToString());
+        while(current.link != null){
+            current = current.link;
+            int temp = int.Parse(current.element.ToString());
+            if(temp > max)
+                max = temp;
+        }
+        return max;
+    }
+    public int Sum(){
+        int S = 0;
+        Node current = header.link;
+        while(current != null){
+            S += int.Parse(current.element.ToString());
+            current = current.link;
+        }
+        return S;
+    }
+    public int Sum2(){
+        Node current = header.link;
+        int S = int.Parse(current.element.ToString());
+        while(current.link != null){
+            current = current.link;
+            S += int.Parse(current.element.ToString());
+        }
+        return S;
+    }
+    public int Count(){
+        Node current = header.link;
+        int count = 1;
+        while(current.link != null){
+            current = current.link;
+            count++;
+        }
+        return count;
+    }
 }
-
-
-public class Program
-{
+public class Program{
     public static void Main(string[] args)
     {
         Console.Clear();
         LinkedList list = new LinkedList();
-        list.Insert("First", "Header");
-        list.Insert("Second", "First");
-        list.Insert("Third", "Second");
+        list.Insert("1", "Header");
+        list.Insert("20", "1");
+        list.Insert("3", "20");
+        list.Insert("9", "3");
+        list.Insert("5", "9");
+        list.Insert("27", "5");
+        list.Insert("7", "27");
         list.Print();
-        Console.WriteLine("===");
-        list.Remove("Second");
-        list.Print();
+        Console.WriteLine("Max: " + list.FindMax());
+        Console.WriteLine("Sum: " + list.Sum());
+        Console.WriteLine("Sum2: " + list.Sum2());
+        Console.WriteLine("Count: " + list.Count());
     }
 }
--- obj/Debug/net8.0/B5.AssemblyInfo.cs
+++ obj/Debug/net8.0/B5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c8dde5f74a92a19a7f7017ff62279ecc937b8b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("B5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
